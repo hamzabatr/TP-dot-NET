bcb03d4c484e0e5d4d336924a7ae3c390291c77e
Remy MENGELLE <r.mengelle@net.estia.fr>
2022-02-15 17:25:59 +0100

    display and create new genre

--- ASP.Server/Controllers/GenreController.cs
+++ ASP.Server/Controllers/GenreController.cs
@@ -6,9 +6,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace ASP.Server.Controllers
 {
+    public class CreateGenreModel
+    {
+        [Required]
+        [Display(Name = "Nom")]
+        public String Name { get; set; }
+    }
+
     public class GenreController : Controller
     {
         private readonly LibraryDbContext libraryDbContext;
@@ -25,12 +33,23 @@ namespace ASP.Server.Controllers
             // récupérer les livres dans la base de donées pour qu'elle puisse être affiché
             if (libraryDbContext.Genre.ToList() != null)
             {
-                 ListGenres = libraryDbContext.Genre.ToList();
+                 ListGenres = libraryDbContext.Genre.Include(g=> g.Books).ToList();
             }else
             {
                 throw new Exception();
             }
             return View(ListGenres);
+        }
+
+        public ActionResult<CreateGenreModel> Create(CreateGenreModel genre)
+        {
+            // Le IsValid est True uniquement si tous les champs de CreateGenreModel marqués Required sont remplis
+            if (ModelState.IsValid)
+            {
+                libraryDbContext.Add(new Genre() {Name=genre.Name});
+                libraryDbContext.SaveChanges();
+            }
+            return View(new CreateGenreModel() {});
         }
     }
 }
