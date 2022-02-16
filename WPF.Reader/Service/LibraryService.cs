3a6449b42a3ef48256fc078e69a4c05b98801ae5
Thomas Ourliac <thomas.ourliac@gmail.com>
2022-02-16 10:54:40 +0100

    Modif vues en cours sur WPF : DetailBook.xaml LibraryService.cs ListBook.cs

--- WPF.Reader/Service/LibraryService.cs
+++ WPF.Reader/Service/LibraryService.cs
@@ -11,10 +11,22 @@ namespace WPF.Reader.Service
         // mais plutot LibraryService.Instance.Books.Add(...)
         // ou LibraryService.Instance.Books.Clear()
         public ObservableCollection<Book> Books { get; set; } = new ObservableCollection<Book>() {
-            new Book(),
-            new Book(),
-            new Book()
+            new Book() { Name = "123"},
+            new Book() { Name = "234"},
+            new Book() { Name = "456"}
         };
+
+        public async void UpdateBookList()
+        {
+            var httpClient = new HttpClient() { BaseAddress = new Uri("https://127.0.0.1:5001") };
+
+            var books = await new ASP.Server.Client(httpClient).ApiBookGetBooksAsync(new List<int>() { 1});
+            Books.Clear();
+            foreach(var book in books.Select(x => new Book() { Name = x.Name }))
+            {
+                Books.Add(book);
+            }
+        }
 
         // C'est aussi ici que vous ajouterez les requète réseau pour récupérer les livres depuis le web service que vous avez fait
         // Vous pourrez alors ajouter les livres obtenu a la variable Books !
